37228c798dd427a084d6ef5def4828484d006889
KazziAzzim <38529205+KazziAzzim@users.noreply.github.com>
2026-04-22 11:32:36 -0400

    Enhance home page with product actions and category carousel

--- src/OnlineBazar/ViewModels/HomeViewModel.cs
+++ src/OnlineBazar/ViewModels/HomeViewModel.cs
@@ -5,4 +5,13 @@ namespace OnlineBazar.ViewModels;
 public class HomeViewModel
 {
     public IEnumerable<ProductDto> FeaturedProducts { get; set; } = new List<ProductDto>();
+    public IEnumerable<ProductDto> CarouselProducts { get; set; } = new List<ProductDto>();
+    public IEnumerable<CategoryProductGroupViewModel> CategoryGroups { get; set; } = new List<CategoryProductGroupViewModel>();
+}
+
+public class CategoryProductGroupViewModel
+{
+    public string CategoryName { get; set; } = string.Empty;
+    public string CategorySlug { get; set; } = string.Empty;
+    public IEnumerable<ProductDto> Products { get; set; } = new List<ProductDto>();
 }
